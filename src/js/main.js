068f1f48e7f30e8f2e6128f83c08e24d00cfe152
John Hopkins <johnthopkins@gmail.com>
2016-10-12 11:09:21 -0700

    working on display items WIP

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,30 @@
-const Game = () => {
-  return true
+import $ from 'jquery'
+let total = 0
+let addPerInterval = 1
+let clickLevel = 1
+
+const totalPlusAddPerInterval = () => {
+  const updateTotal = () => {
+    let score = document.getElementById('current-score')
+    total += parseInt(addPerInterval)
+    score.innerHTML = ""
+    score.insertAdjacentHTML('afterbegin', `${total}`)
+  }
+  window.setInterval(updateTotal,1000)
+
 }
 
-export default Game
+const updateAddPerInterval = (sumOfPurchasedUnits) => {
+  addPerInterval += sumOfPurchasedUnits
+}
+
+const totalPlusClickLevel = () => {
+  total += clickLevel
+}
+
+const increaseClickLevel = () => {
+  return clickLevel
+}
+
+totalPlusAddPerInterval()
+// updateAddPerInterval(2)
