8d5ceab9f66739999e845e3270f298a1e78fffc6
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-12 10:13:42 -0700

    setCookie, getCookie working

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -1,13 +1,16 @@
 const setCookie = (gamedata) => {
   let jsonString = JSON.stringify(gamedata)
-  let cookieString = 'gamesave='+ gamedata + ';expires=10000'
+  let cookieString = 'gamesave='+ jsonString + ';expires=10000'
   document.cookie = cookieString
 }
 
-const getCookie = (cookie) => {
-  function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
+const getCookie = () => {
+  let savedGameData = ''
+  let value = "; " + document.cookie;
+  let parts = value.split("; gamesave=");
+  if (parts.length == 2) {
+    savedGameData = parts.pop().split(";").shift();
   }
+  savedGameData = JSON.parse(savedGameData)
+  return savedGameData
 }
