7516575cb2bb2d3ddf082bdf540202c8c089ba5a
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-12 15:58:43 -0700

    added buy div, setup css structure and created divController functionality

--- src/js/divController.js
+++ src/js/divController.js
@@ -1,8 +1,8 @@
-import $ from 'jquery';
+import $ from 'jquery'
 
-alert('test')
-
-$('#buyArea').click(function() {
-  alert('youClicked')
-  $('#buyArea').hide()
+$(document).ready(function () {
+  $('#buy-shapes').click(function(){
+    $('#dimmer').fadeIn()
+    $('#buyArea').fadeIn()
+  })
 })
