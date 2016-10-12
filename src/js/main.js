a3458214e06115c4f3661abf1771f9486cebe9eb
John Hopkins <johnthopkins@gmail.com>
2016-10-12 15:08:52 -0700

    trying to pull in build script

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,19 @@
 let total = 0
 let addPerInterval = 1
 let clickLevel = 1
+import {Shape, Triangle, Square, Circle, Hexagon} from './shapes.js'
+const shapes = []
 
 const totalPlusAddPerInterval = () => {
   const updateTotal = () => {
     let score = document.getElementById('current-score')
     total += parseInt(addPerInterval)
-    score.innerHTML = ""
-    score.insertAdjacentHTML('afterbegin', `${total}`)
+    document.getElementById('current-score').innerHTML = ""
+    document.getElementById('current-score').insertAdjacentHTML('afterbegin', `${total}`)
+    document.getElementById('profit-per-second').innerHTML = ""
+    document.getElementById('profit-per-second').insertAdjacentHTML('afterbegin', `${addPerInterval}`)
+    document.getElementById('profit-per-click').innerHTML = ""
+    document.getElementById('profit-per-click').insertAdjacentHTML('afterbegin', `${clickLevel}`)
   }
   window.setInterval(updateTotal,1000)
 
@@ -22,8 +28,13 @@ const totalPlusClickLevel = () => {
 }
 
 const increaseClickLevel = () => {
-  return clickLevel
+  clickLevel
 }
+
+const buyShape = (shape) => {
+
+}
+
 
 totalPlusAddPerInterval()
 updateAddPerInterval(2)
